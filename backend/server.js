f2f3ec597218e04181200ad3ec5721c1a9ce298f
greatooau <pajaroazulx@gmail.com>
2022-03-01 09:16:10 -0600

    routes created

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("hola")
+const express = require('express');
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+app.get('/api/goals', (req,res) => {
+    res.status(200).json({
+        mamasita: "la mamasita está bien nalgona"
+    })
+})
